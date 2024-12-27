b1f4288245c136fc40d7f4af1296bdc6a36175f3
OnsKoukiPro <ons.koukipro@yahoo.com>
2024-12-27 13:51:50 +0100

    fixed app routing

--- client/school-platform/src/app/app-routing.module.ts
+++ client/school-platform/src/app/app-routing.module.ts
@@ -1,29 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {DashboardOverviewComponent}  from './pages/dashboard/overview/dashboard-overview.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AddStudentComponent } from './pages/dashboard/student/addstudent/addstudent.component';
-import { ListStudentsComponent } from './pages/dashboard/student/liststudents/liststudents.component';
-import { AddTeacherComponent } from './pages/dashboard/teacher/addteacher/addteacher.component';
-import { AddClassComponent } from './pages/dashboard/class/addclass/addclass.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard/overview', pathMatch: 'full' },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    children: [
-      { path: 'overview', component: DashboardOverviewComponent }, // New Overview Component
-      { path: 'student/addstudent', component: AddStudentComponent },
-      { path: 'student/liststudents', component: ListStudentsComponent },
-      { path: 'teacher/addteacher', component: AddTeacherComponent },
-      { path: 'class/addclass', component: AddClassComponent },
-    ],
-  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- client/school-platform/src/app/app.module.ts
+++ client/school-platform/src/app/app.module.ts
@@ -1,47 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CardComponent } from './components/card/card.component';
-import { FeatureCardsComponent } from './components/feature-card/feature-card.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { AbsenceComponent } from './components/absence/absence.component';
-import { AddStudentComponent } from './pages/dashboard/student/addstudent/addstudent.component';
-import { AddTeacherComponent } from './pages/dashboard/teacher/addteacher/addteacher.component';
-import { AddClassComponent } from './pages/dashboard/class/addclass/addclass.component';
-import { ListStudentsComponent } from './pages/dashboard/student/liststudents/liststudents.component';
+import { DashboardModule } from '../app/pages/dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
-import { DashboardOverviewComponent } from './pages/dashboard/class/pages/dashboard/overview/dashboard-overview/dashboard-overview.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent,
-    HeaderComponent,
-    DashboardComponent,
-    CardComponent,
-    CalendarComponent,
-    FeatureCardsComponent,
-    AbsenceComponent,
-    AddStudentComponent,
-    AddTeacherComponent,
-    AddClassComponent,
-    ListStudentsComponent,
-    DashboardOverviewComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    DashboardModule,
+    SharedModule,
+    HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
