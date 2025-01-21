a5e045b7eb5fa16f20f1b406c50e71af240f4d4f
ahmedhoucine <ahmed.houcine@insat.ucar.tn>
2025-01-21 12:29:55 +0100

    add teacher(still not working)

--- client/school-platform/src/app/pages/dashboard/teacher/addteacher/addteacher.component.ts
+++ client/school-platform/src/app/pages/dashboard/teacher/addteacher/addteacher.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Matiere } from '../../../../components/utils/matiere.enum';
+import { TeacherService } from 'src/app/services/teacher.service';
 
 @Component({
   selector: 'app-add-teacher',
@@ -11,7 +12,7 @@ export class AddTeacherComponent implements OnInit {
   teacherForm!: FormGroup;
   matiereOptions = Object.values(Matiere);
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,private teacherservice : TeacherService) {}
 
   ngOnInit(): void {
     this.teacherForm = this.fb.group({
@@ -43,8 +44,20 @@ export class AddTeacherComponent implements OnInit {
   onSubmit(): void {
     if (this.teacherForm.valid) {
       console.log(this.teacherForm.value);
+      const teachertData = this.teacherForm.value;
+
+      this.teacherservice.createTeacher(teachertData).subscribe(
+        (response) => {
+          console.log('Student created successfully', response);
+        },
+        (error) => {
+          console.error('Error creating student', error);
+        }
+      );
+     
     } else {
       console.log('Form is invalid');
     }
+  
   }
 }
