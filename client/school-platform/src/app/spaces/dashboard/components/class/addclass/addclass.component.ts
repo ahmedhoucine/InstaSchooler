1377aa2149197b91c30cca851de494c08a4c3d0f
OnsKoukiPro <ons.koukipro@yahoo.com>
2025-01-30 22:34:04 +0100

    added form validation

--- client/school-platform/src/app/spaces/dashboard/components/class/addclass/addclass.component.ts
+++ client/school-platform/src/app/spaces/dashboard/components/class/addclass/addclass.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Matiere } from '../../utils/matiere.enum';  // Assuming Matiere is an Enum
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Matiere } from '../../utils/matiere.enum';
 import { TeacherService } from '../../../services/teacher.service';
 import { ClassService } from '../../../services/class.service';
 import { Router } from '@angular/router';
@@ -14,8 +14,8 @@ export class AddClassComponent implements OnInit {
   classForm!: FormGroup;
   levels = [1, 2, 3, 4];
   matiereOptions = Object.values(Matiere);
-  allTeachers: any[] = []; // Store all teachers
-  teachers: any[] = []; // Filtered teachers based on selected subject
+  allTeachers: any[] = [];
+  teachers: any[] = [];
 
   constructor(
     private teacherService: TeacherService,
@@ -26,16 +26,17 @@ export class AddClassComponent implements OnInit {
 
   ngOnInit(): void {
     this.classForm = this.fb.group({
-      description: ['', Validators.required],
-      subject: ['', Validators.required],
-      niveau: [1, Validators.required],
-      teacher: ['', Validators.required],
-      duration: ['', [Validators.required, Validators.min(1)]]
+      description: new FormControl('', Validators.required),
+      subject: new FormControl('', Validators.required),
+      niveau: new FormControl(1, Validators.required),
+      teacher: new FormControl('', Validators.required),
+      duration: new FormControl('', [Validators.required, Validators.min(1)])
     });
 
     this.getAllTeachers();
 
     this.classForm.get('subject')?.valueChanges.subscribe((selectedSubject) => {
+      this.classForm.get('teacher')?.setValue('');
       this.fetchTeachersBySubject(selectedSubject);
     });
   }
@@ -45,7 +46,6 @@ export class AddClassComponent implements OnInit {
       (data) => {
         this.allTeachers = data;
         this.teachers = [...this.allTeachers];
-        console.log("All teachers:", this.allTeachers);
       },
       (error) => {
         console.error('Error fetching teachers', error);
@@ -66,20 +66,21 @@ export class AddClassComponent implements OnInit {
   }
 
   onSubmit(): void {
-    if (this.classForm.valid) {
-      console.log("Class Data:", this.classForm.value);
-      this.classService.createClass(this.classForm.value).subscribe(
-        (response) => {
-          console.log('Class created successfully:', response);
-          this.router.navigate(['/dashboard/class/listclasses']);
-        },
-        (error) => {
-          console.error('Error creating class', error);
-        }
-      );
-
-    } else {
-      console.log('Form is invalid');
+    if (this.classForm.invalid) {
+      this.classForm.markAllAsTouched();
+      return;
     }
+
+    console.log("Class Data:", this.classForm.value);
+    this.classService.createClass(this.classForm.value).subscribe(
+      (response) => {
+        console.log('Class created successfully:', response);
+        this.router.navigate(['/dashboard/class/listclasses']);
+      },
+      (error) => {
+        console.error('Error creating class', error);
+      }
+    );
   }
+
 }
