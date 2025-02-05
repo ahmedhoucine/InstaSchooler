288e91d38947a8816113d1ce37da0a94b6431d2f
fatmabouhari <fatma.bouhari@insat.ucar.tn>
2025-02-05 16:02:18 +0100

    changes

--- client/school-platform/src/app/spaces/teacher_space/navbar/navbar.component.ts
+++ client/school-platform/src/app/spaces/teacher_space/navbar/navbar.component.ts
@@ -16,6 +16,6 @@ export class NavbarComponent {
 
   logout(): void {
     localStorage.removeItem('authToken');
-    this.router.navigate(['/login']);
+    this.router.navigate(['/join']);
   }
 }
--- client/school-platform/src/app/spaces/teacher_space/teacher-routing.module.ts
+++ client/school-platform/src/app/spaces/teacher_space/teacher-routing.module.ts
@@ -6,31 +6,28 @@ import { CoursesComponent } from './courses/courses.component';
 import { ListeComponent } from './liste/liste.component';
 import { NotificationComponent } from './notification/notification.component';
 import { SettingsComponent } from './settings/settings.component';
-import { LoginComponent } from './login/login.component'; // Assurez-vous que ce chemin est correct
-import { AddCourseComponent } from './add-course/add-course.component'; // Ajouter le chemin vers AddCourseComponent
-import { AddTaskComponent } from './add-task/add-task.component'; // Ajouter le chemin vers AddTaskComponent
-import { TeacherRegisterComponent } from './teacher-register/teacher-register.component'; // Ajouter le chemin vers AddTaskComponent
+import { LoginComponent } from './login/login.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { TeacherRegisterComponent } from './teacher-register/teacher-register.component';
 import { StudentComponent } from './student/student.component';
-
-
+import { TeacherGuard } from '../../core/guards-teacher/teacher.guard'; 
 const routes: Routes = [
   {
     path: '',
     component: TeacherComponent,
     children: [
       { path: '', redirectTo: 'dashboards', pathMatch: 'full' },
-      { path: 'dashboards', component: DashboardComponent },
-      { path: 'courses', component: CoursesComponent },
-      { path: 'liste', component: ListeComponent },
-      { path: 'notifications', component: NotificationComponent },
-      { path: 'settings', component: SettingsComponent },
-      { path: 'login', component: LoginComponent }, // Route pour le login
-      { path: 'add-course', component: AddCourseComponent }, // Route pour ajouter un cours
-      { path: 'add-task', component: AddTaskComponent }, // Route pour ajouter une tâche
-      { path: 'teacher-register', component: TeacherRegisterComponent }, // Route pour ajouter une tâche
-      { path: 'student', component: StudentComponent }, // Route pour ajouter une tâche
-
-
+      { path: 'dashboards', component: DashboardComponent, canActivate: [TeacherGuard] },
+      { path: 'courses', component: CoursesComponent, canActivate: [TeacherGuard] },
+      { path: 'liste', component: ListeComponent, canActivate: [TeacherGuard] },
+      { path: 'notifications', component: NotificationComponent, canActivate: [TeacherGuard] },
+      { path: 'settings', component: SettingsComponent, canActivate: [TeacherGuard] },
+      { path: 'add-course', component: AddCourseComponent, canActivate: [TeacherGuard] },
+      { path: 'add-task', component: AddTaskComponent, canActivate: [TeacherGuard] },
+      { path: 'teacher-register', component: TeacherRegisterComponent, canActivate: [TeacherGuard] },
+      { path: 'student', component: StudentComponent, canActivate: [TeacherGuard] },
+      { path: 'login', component: LoginComponent }, 
     ],
   },
 ];
